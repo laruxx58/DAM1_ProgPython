21d8b3b56b8ca4bb20bc77df9643c2512da4446b
laruxx58 <145799968+laruxx58@users.noreply.github.com>
2023-10-09 20:52:28 +0200

    Update Act2.1.py

--- EjercicionsU1/Act2.1.py
+++ EjercicionsU1/Act2.1.py
@@ -1,5 +1,5 @@
 #Ejercico 2.1
 #Escribe un programa que pida el nombre del usuario para luego darle la bienvenida.
 print ("Escribe tu nombre: ")
-nombre=input()
-print ("Hola,", nombre)
+nom=input()
+print ("Hola,", nom)
