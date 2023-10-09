8e5453817d776c929e536909faa7439b50c659b3
Lara Narvaez <lnarote@g.educaand.es>
2023-10-09 21:41:08 +0200

    act 23

--- EjercicionsU1/Act2.23.py
+++ EjercicionsU1/Act2.23.py
@@ -2,3 +2,6 @@
 #Escribir un programa que pregunte el correo electrónico del usuario en la consola 
 #y muestre por pantalla otro correo electrónico con el mismo nombre (la parte delante de la arroba @) pero con dominio ceu.es.
 email=str(input("Ingrese su email: "))
+correo= email.split("@")[0]
+nuevoemail= correo + "@ceu.es"
+print("Su nuevo email es: " , nuevoemail)
