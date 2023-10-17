4cb3937b931e4df07126ebb37346f52b76d028c5
Lara Narvaez <lnarote@g.educaand.es>
2023-10-17 20:53:30 +0200

    act 11

--- EjercicionsU1/Entrega1.9/Act1.11_def.py
+++ EjercicionsU1/Entrega1.9/Act1.11_def.py
@@ -1,5 +1,10 @@
 #Ejercicio 1.11
 #Recibe un número y retorna una cadena de caracteres con el resultado de la función.
 
-def resultado():
-    return 
+def resultado(num):
+    total=n*(n+1)/2
+    return "La suma de los primeros enteros hasta " + str(num) + " es " + str(total)
+
+
+n=int(input("Introduce un numero entero positivo: "))
+print(resultado(n))
